26bee5e4358481e1d5f9fdb17759c799c7e60899
Tyler Long <tyler4long@gmail.com>
2014-12-03 22:10:21 +0800

    Change project structure

--- User32/Properties/AssemblyInfo.cs
+++ User32/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DllImport")]
+[assembly: AssemblyTitle("User32")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DllImport")]
+[assembly: AssemblyProduct("User32")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5d3001b2-cf08-48c8-be72-de8959ed1ea9")]
+[assembly: Guid("1977ccab-d017-42aa-940c-3cb6607e537b")]
 
 // Version information for an assembly consists of the following four values:
 //
