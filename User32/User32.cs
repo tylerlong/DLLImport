0089e541d1176c47d179f5df6ea48f06ab1471b1
Tyler Long <tyler4long@gmail.com>
2014-12-03 17:23:07 +0800

    Add some methods to user32

--- User32/User32.cs
+++ User32/User32.cs
@@ -5,5 +5,39 @@ namespace DllImport
 {
     public static class User32
     {
+        public const int GWL_STYLE = -16;
+        public const int GWL_HWNDPARENT = -8;
+        public const int WS_CAPTION = 0x00C00000;
+        public const int WS_SIZEBOX = 0x00040000;
+
+        [DllImport("User32.dll")]
+        public static extern IntPtr GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("User32.dll")]
+        public static extern IntPtr GetWindowLongPtr(IntPtr hWnd, int nIndex);
+
+        [DllImport("User32.dll")]
+        public static extern long SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("User32.dll")]
+        public static extern long SetWindowLongPtr(IntPtr hWnd, int nIndex, int dwNewLong);
+
+
+
+        public const int HWND_NOTOPMOST = -2;
+        public const int SWP_NOACTIVATE = 0x0010;
+        public const int SWP_NOMOVE = 0x0002;
+        public const int SWP_NOSIZE = 0x0001;
+        public const int SWP_NOZORDER = 0x0004;
+
+        [DllImport("User32.dll")]
+        public static extern IntPtr SetWindowPos(IntPtr handle, int handleInsertAfter, int x, int Y, int width, int height, int uFlags);
+
+
+
+        public const int SW_RESTORE = 9;
+
+        [DllImport("User32.dll")]
+        public static extern bool ShowWindow(IntPtr handle, int nCmdShow);
     }
 }
