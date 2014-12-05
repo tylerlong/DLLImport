580157cff6336cac4995276ab9acac3feb2ffba9
Tyler Long <tyler4long@gmail.com>
2014-12-05 23:31:40 +0800

    Add User32 IsWindowVisible

--- User32/User32.cs
+++ User32/User32.cs
@@ -6,9 +6,12 @@ namespace DLLImport
     public static class User32
     {
         public const int GWL_STYLE = -16;
+        public const int GWL_EXSTYLE = -20;
         public const int GWL_HWNDPARENT = -8;
         public const int WS_CAPTION = 0x00C00000;
         public const int WS_SIZEBOX = 0x00040000;
+        public const int WS_EX_TOOLWINDOW = 0x00000080;
+        public const int WS_EX_APPWINDOW = 0x00040000;
 
         [DllImport("User32.dll")]
         public static extern IntPtr GetWindowLong(IntPtr hWnd, int nIndex);
@@ -31,19 +34,24 @@ namespace DLLImport
         public const int SWP_NOZORDER = 0x0004;
 
         [DllImport("User32.dll")]
-        public static extern IntPtr SetWindowPos(IntPtr handle, int handleInsertAfter, int x, int Y, int width, int height, int uFlags);
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int handleInsertAfter, int x, int Y, int width, int height, int uFlags);
 
 
-
+        public const int SW_HIDE = 0;
+        public const int SW_SHOW = 5;
         public const int SW_RESTORE = 9;
 
         [DllImport("User32.dll")]
-        public static extern bool ShowWindow(IntPtr handle, int nCmdShow);
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
         
 
         public delegate bool WndEnumProc(IntPtr hwnd, IntPtr lParam);
         [DllImport("User32.dll")]
         public static extern bool EnumWindows(WndEnumProc lpEnumFunc, IntPtr lParam);
+
+
+        [DllImport("User32.dll")]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
     }
 }
