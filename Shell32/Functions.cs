ec300db05d8e4c26c71c6285e0e0d98e4973cb51
Tyler Long <tyler4long@gmail.com>
2015-02-27 11:26:03 +0800

    Add method SHGetFileInfo

--- Shell32/Functions.cs
+++ Shell32/Functions.cs
@@ -1,7 +1,11 @@
-﻿
+﻿using System;
+using System.Runtime.InteropServices;
+
 namespace DLLImport
 {
     public static partial class Shell32
     {
+        [DllImport("Shell32.dll")]
+        public static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbFileInfo, uint uFlags);
     }
 }
---
+++ Shell32/Structs.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace DLLImport
+{
+    public static partial class Shell32
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SHFILEINFO
+        {
+            public IntPtr hIcon;
+            public int iIcon;
+            public uint dwAttributes;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szDisplayName;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
+            public string szTypeName;
+        };
+    }
+}
